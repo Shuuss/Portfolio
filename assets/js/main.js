c19aa5dbbdf1b7fa7bf27672b0e060ebbee36bd8
Shuuss <tanguyabdoulvaid@gmail.com>
2024-12-04 20:37:25 +0100

    filter portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,3 +18,46 @@ const tabs = document.querySelectorAll('[data-target]'),
 				tab.classList.add('skills__active')
 			})
 		})
+
+		document.addEventListener("click",(e) => {
+			if(e.target.classList.contains("work__button")){
+				togglePortfolioPopup();
+				portfolioItemsDetails(e.target.parentElement);
+			}
+		})
+
+		function togglePortfolioPopup(){
+			document.querySelector(".portfolio__popup").classList.toggle("open");
+		}
+
+		document.querySelector(".portfolio__popup-close").addEventListener("click",togglePortfolioPopup);
+
+
+		function portfolioItemsDetails(portfolioItem){
+			document.querySelector(".pp__thumbnail img").src = portfolioItem.querySelector(".work__img").src;
+			document.querySelector(".portfolio__popup-subtitle span").innerHTML = portfolioItem.querySelector(".work__title").innerHTML;
+			document.querySelector(".portfolio__popup-body").innerHTML = portfolioItem.querySelector(".portfolio__item-details").innerHTML;
+
+		}
+
+		var mixerPortfolio = mixitup('.work__container', {
+    controls: {
+        toggleDefault: 'none'
+    },
+    selectors :{
+    	target: '.work__card'
+    },
+    animation :{
+    	duration: 300
+    }
+});
+
+
+const linkWork = document.querySelectorAll('.work__item')
+
+function activeWork(){
+	linkWork.forEach(l=> l.classList.remove('active-work'))
+	this.classList.add('active-work')
+}
+
+linkWork.forEach(l=> l.addEventListener("click", activeWork))
